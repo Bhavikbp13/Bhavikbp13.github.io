6ca1e3f155af7478f23d77b3dbc86fac730ac31b
Bhavik Patel <bhavikbp13@gmail.com>
2026-07-13 15:49:58 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/BHAVIKBP13.GITHUB.IO/' // Change this to '/BHAVIKBP13.GITHUB.IO/' if GitHub Pages requires it later
+  base: '/' // Change this to '/BHAVIKBP13.GITHUB.IO/' if GitHub Pages requires it later
 })
