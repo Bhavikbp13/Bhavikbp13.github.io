752b8d865e946bb037d5db322c05e692215af596
Bhavik Patel <bhavikbp13@gmail.com>
2026-07-13 15:45:39 -0400

    Fixed home page

--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// Make sure your components are imported!
+import HomeView from './views/HomeView.vue' 
 import AboutView from './views/AboutView.vue'
 import ProjectsView from './views/ProjectsView.vue'
 import SkillsView from './views/SkillsView.vue'
 import ContactView from './views/ContactView.vue'
 
 const routes = [
+  { path: '/', name: 'home', component: HomeView }, 
   { path: '/about', name: 'about', component: AboutView },
   { path: '/projects', name: 'projects', component: ProjectsView },
   { path: '/skills', name: 'skills', component: SkillsView },
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home-page">
+  </div>
+</template>
+
+<script setup>
+// Import your components so this page can use them!
+import Hero from '../components/Hero.vue'
+import Stats from '../components/Stats.vue'
+</script>
