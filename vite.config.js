73ed52456b5fe318e14bbcc15f68c9b06436c8c7
Bhavik Patel <bhavikbp13@gmail.com>
2026-07-13 23:37:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './', // Keep this
-  build: {
-    // This ensures the assets are bundled correctly
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+  base: './',
 })
